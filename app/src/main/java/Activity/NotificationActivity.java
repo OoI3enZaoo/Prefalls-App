75aca4f9e41f280ea3ad02304c2d520230d256ca
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2017-06-15 14:11:33 +0700

    -Change color at PetientListActivity when message is sent

--- app/src/main/java/Activity/NotificationActivity.java
+++ app/src/main/java/Activity/NotificationActivity.java
@@ -2,29 +2,36 @@ package Activity;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import SQLite.DBAlertAll;
+import SQLite.DBAlertEachOne;
 import SQLite.DBPetient;
+import SQLite.DBUser;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NotificationActivity extends AppCompatActivity {
@@ -36,12 +43,30 @@ public class NotificationActivity extends AppCompatActivity {
     ArrayList<String> mNameUserArray = new ArrayList<>();
     ArrayList<String> mNameTypeArray = new ArrayList<>();
     ArrayList<String> mImagePathArray = new ArrayList<>();
-    ArrayList<String> mTimeArray= new ArrayList<>();
+    ArrayList<String> mTimeArray = new ArrayList<>();
+    ArrayList<String> mLatArray = new ArrayList<>();
+    ArrayList<String> mLngArray = new ArrayList<>();
+    ArrayList<String> mColorArray = new ArrayList<>();
     public static CardViewAdapter mAdapter;
+    public String PID = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String type = intent.getStringExtra("type");
+        Log.i(TAG, "TYPE: " + type);
+        if (type.equals("all")) {
+            PID = null;
+            Toast.makeText(getApplicationContext(), "all", Toast.LENGTH_SHORT).show();
+        } else {
+
+            PID = type;
+            Toast.makeText(getApplicationContext(), "each", Toast.LENGTH_SHORT).show();
+        }
         mIntentFilter = new IntentFilter();
         mIntentFilter.addAction(mBroadcastStringAction);
         mIntentFilter.addAction(mBroadcastIntegerAction);
@@ -58,8 +83,16 @@ public class NotificationActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onButton1Click(View view, int position) {
+            public void onLocationClick(View view, int position) {
                 Toast.makeText(getApplicationContext(), "Clicked Button1 in " + mNameUserArray.get(position), Toast.LENGTH_SHORT).show();
+                Bundle bundle = new Bundle();
+                bundle.putString("lat", mLatArray.get(position));
+                bundle.putString("lng", mLngArray.get(position));
+                Log.i(TAG, "LAT: " + mLatArray.get(position));
+                Log.i(TAG, "LONG: " + mLngArray.get(position));
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
 
             @Override
@@ -77,20 +110,73 @@ public class NotificationActivity extends AppCompatActivity {
         mAdapter.notifyDataSetChanged();
 
 
-
     }
+
     @Override
     public void onResume() {
         super.onResume();
         registerReceiver(mReceiver, mIntentFilter);
+        Log.i(TAG, "onReSume()");
+        mNameUserArray.clear();
+        mNameTypeArray.clear();
+        mImagePathArray.clear();
+        mTimeArray.clear();
+        mLatArray.clear();
+        mLngArray.clear();
+        mColorArray.clear();
+        if (PID == null) {
+            Log.i(TAG, "PID == null");
+            DBAlertAll dbAlertAll = new DBAlertAll(getApplicationContext());
+            Cursor res = dbAlertAll.getAllData();
+
+            Log.i(TAG, "res.getCount() " + res.getCount());
+            if (res.getCount() == 0) {
+                Log.i(TAG, "Nothing found");
+            } else {
+                while (res.moveToNext()) {
+                    mNameUserArray.add(res.getString(0));
+                    mNameTypeArray.add(res.getString(1));
+                    mImagePathArray.add(res.getString(2));
+                    mTimeArray.add(res.getString(3));
+                    mLatArray.add(res.getString(4));
+                    mLngArray.add(res.getString(5));
+                    mColorArray.add(res.getString(6));
+                }
+            }
+
+        } else {
+            DBAlertEachOne dbAlertEachOne = new DBAlertEachOne(getApplicationContext());
+
+            Cursor res = dbAlertEachOne.getAllData(PID);
+            ;
+            Log.i(TAG, "dbAlertEachOne.getCount(): " + res.getCount());
+            if (res.getCount() == 0) {
+                Log.i(TAG, "DBAlertEachOne == 0");
+            } else {
+                while (res.moveToNext()) {
+                    mNameUserArray.add(res.getString(0));
+                    mNameTypeArray.add(res.getString(1));
+                    mImagePathArray.add(res.getString(2));
+                    mTimeArray.add(res.getString(3));
+                    mLatArray.add(res.getString(4));
+                    mLngArray.add(res.getString(5));
+                    mColorArray.add(res.getString(6));
+                }
+            }
+
+            Toast.makeText(this, "PID:" + PID, Toast.LENGTH_SHORT).show();
+        }
+        mAdapter.notifyDataSetChanged();
+
     }
+
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String pid = intent.getStringExtra("pid").toString();
             String fullname = intent.getStringExtra("fullname").toString();
             String typename = intent.getStringExtra("typename").toString();
-
+            String timestart = intent.getStringExtra("timestart").toString();
             DBPetient dbPetient = new DBPetient(getApplicationContext());
             String imagepath = dbPetient.getImagepath(pid);
 
@@ -99,14 +185,12 @@ public class NotificationActivity extends AppCompatActivity {
             Log.i(TAG, "fullname: " + fullname);
             Log.i(TAG, "typename: " + typename);
             Log.i(TAG, "imagepath: " + imagepath);
+            Log.i(TAG, "timestart: " + timestart);
 
-            mNameUserArray.add(fullname);
-            mNameTypeArray.add(typename);
-            mImagePathArray.add(imagepath);
-            mTimeArray.add("time");
-            mAdapter.notifyDataSetChanged();
+
         }
     };
+
     @Override
     protected void onPause() {
         unregisterReceiver(mReceiver);
@@ -136,10 +220,14 @@ public class NotificationActivity extends AppCompatActivity {
 
             Picasso.with(getApplicationContext()).load("http://sysnet.utcc.ac.th/prefalls/images/patients/" + mImagePathArray.get(i)).into(viewHolder.imgProfile);
             viewHolder.txtName.setText(mNameUserArray.get(i));
-            viewHolder.txtStatus.setText("Status: normal");
-            viewHolder.txtTstamp.setText("time");
+
+            /*Long time = Long.parseLong(mTimeArray.get(i));
+            Date date = new Date(time*1000L); // *1000 is to convert seconds to milliseconds
+            String sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(date);*/
+
+            viewHolder.txtTstamp.setText(mTimeArray.get(i));
             viewHolder.txtStatus.setText(mNameTypeArray.get(i));
-            viewHolder.constraintLayout.setBackgroundColor(Color.parseColor("#b3e0ff"));
+            viewHolder.constraintLayout.setBackgroundColor(Color.parseColor(mColorArray.get(i)));
 
 
         }
@@ -157,6 +245,7 @@ public class NotificationActivity extends AppCompatActivity {
             private TextView txtStatus;
             private TextView txtTstamp;
             private ConstraintLayout constraintLayout;
+            private ImageButton btnLocation;
 
             public ViewHolder(View itemView) {
                 super(itemView);
@@ -164,23 +253,87 @@ public class NotificationActivity extends AppCompatActivity {
                 imgProfile = (CircleImageView) itemView.findViewById(R.id.img_profile);
                 txtStatus = (TextView) itemView.findViewById(R.id.txtstatus);
                 constraintLayout = (ConstraintLayout) itemView.findViewById(R.id.constraintBackground);
-                txtTstamp = (TextView)itemView.findViewById(R.id.tstamp);
-
+                txtTstamp = (TextView) itemView.findViewById(R.id.tstamp);
+                btnLocation = (ImageButton) itemView.findViewById(R.id.btnLocation);
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         onItemTouchListener.onCardViewTap(v, getLayoutPosition());
                     }
                 });
+                btnLocation.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        /*int position=(Integer)v.getTag();
+                        Log.i(TAG,"position: " + position);*/
+                        onItemTouchListener.onLocationClick(v, getLayoutPosition());
+
+                    }
+                });
             }
         }
     }
+
     interface OnItemTouchListener {
 
         void onCardViewTap(View view, int position);
 
-        void onButton1Click(View view, int position);
+        void onLocationClick(View view, int position);
 
         void onButton2Click(View view, int position);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.icon_bin:
+
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(NotificationActivity.this);
+                builder.setMessage("Are you sure you want to remove all notification?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(),
+                                "Remove", Toast.LENGTH_SHORT).show();
+                        mNameUserArray.clear();
+                        mNameTypeArray.clear();
+                        mImagePathArray.clear();
+                        mTimeArray.clear();
+                        mLatArray.clear();
+                        mLngArray.clear();
+                        mColorArray.clear();
+                        mAdapter.notifyDataSetChanged();
+                       // if(PID == null){
+                            DBAlertAll dbAlertAll = new DBAlertAll(getApplicationContext());
+                            dbAlertAll.deleteData();
+                       // }else{
+                            DBAlertEachOne dbAlertEachOne = new DBAlertEachOne(getApplicationContext());
+                            dbAlertEachOne.deleteData();
+                       // }
+
+
+
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notification, menu);
+        return true;
     }
 }
